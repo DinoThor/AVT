a55cabd028645b99cb92e48f692c2080cc4ae953
DinoThor <Aaron.Raya.Lopez@gmail.com>
2023-02-13 13:13:20 +0100

    unificación ui main window

--- app/main.js
+++ app/main.js
@@ -3,14 +3,12 @@ const { connection, insertDetail, createUser, updateAnalisis } = require('../db/
 const path = require('path')
 
 const statik = require('@brettz9/node-static');
+const { mainModule } = require('process');
 const file = new statik.Server(path.join(__dirname, '../sdk'), { cache: 0 })
 const filepath = '../db/database.db'
 
 var db = connection(filepath)
-var windows = {
-  settWin: [null, '../www/settings.html'],
-  userWin: [null, '../www/newuser.html']
-}
+var mainWindow
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
@@ -30,19 +28,17 @@ function createSdk() {
 }
 
 
-function createWin(type) {
-  win = windows[type][0]
-  windows[type][0] = new BrowserWindow({
-    width: 800, height: 600,
+function createMainWin() {
+  mainWindow = new BrowserWindow({
+    width:1240, height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
     show: false,
     titleBarStyle: 'hidden'
   })
-  windows[type][0].loadFile(path.join(__dirname, windows[type][1]))
-  windows[type][0].on('closed', () => windows[type][0] = null)
-  windows[type][0].webContents.on('did-finish-load', () => windows[type][0].show())
+  mainWindow.on('closed', () => mainWindow = null)
+  mainWindow.loadFile(path.join(__dirname, '../www/index.html'))
 }
 
 
@@ -50,6 +46,7 @@ app.whenReady().then(() => {
   createSdk()
   createTray()
   initIpc()
+  createMainWin()
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -63,25 +60,11 @@ function createTray() {
   var tray = new Tray(path.join(__dirname, '../assets/tray.ico'))
   tray.setToolTip('MorphCast VRAIN')
   const contextMenu = Menu.buildFromTemplate([
-    // {
-    //   label: 'Abrir ventana',
-    //   click: () => {
-    //     mainWindow.show()
-    //   }
-    // },
     {
-      label: 'Abrir ajustes',
-      click: (() => {
-        if (windows['settWin'][0] == null) createWin('settWin')
-        else windows['settWin'][0].focus()
-      })
-    },
-    {
-      label: 'Crear usuario',
-      click: (() => {
-        if (windows['userWin'][0] == null) createWin('userWin')
-        else windows['userWin'][0].focus()
-      })
+      label: 'Abrir',
+      click: () => {
+        mainWindow.show()
+      }
     },
     {
       label: 'Cerrar',
@@ -96,8 +79,9 @@ function createTray() {
 }
 
 function initIpc() {
-  ipcMain.on('close-win', (e, type) => windows[type][0].close())
+  ipcMain.on('close-win', (e) => mainWindow.hide())
 
+  // Data API
   ipcMain.on('new-data', (e, values) => insertDetail(db, 1, values))
   ipcMain.on('new-user', (e, values) => createUser(db, values))
   ipcMain.on('update-analisis', (e) => updateAnalisis(db, 1))
